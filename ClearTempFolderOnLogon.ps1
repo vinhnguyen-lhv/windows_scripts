ce7bf2dfa96dd87c5f6cc3ad2fb37a14d7f1ac3c
Vinh Nguyen <nvqvinh@gmail.com>
2024-12-02 21:12:16 +0700

    First

---
+++ ClearTempFolderOnLogon.ps1
@@ -0,0 +1,12 @@
+﻿# Array of folder paths
+$folderPaths = @(
+    "C:\Users\vinh\Downloads\TEMP",
+    "C:\Users\vinh\AppData\Local\Temp"
+    # Add more folder paths as needed
+)
+
+# Loop through each folder path
+foreach ($folderPath in $folderPaths) {
+    # Clear the contents of the folder
+    Get-ChildItem -Path $folderPath | Remove-Item -Force -Recurse
+}
